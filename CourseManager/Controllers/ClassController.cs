645e6f19867e084592bd2b88a95be6117262a3bd
fxd <2840391409@qq.com>
2019-06-23 17:16:49 +0800

    create

---
+++ CourseManager/Controllers/ClassController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CourseManager.Models;
+using CourseManager.CourseManagerEntities__CourseManager;
+
+namespace CourseManager.Controllers
+{ 
+    public class ClassController : Controller
+    {
+        private Models_ db = new Models_();
+
+        //
+        // GET: /Class/
+
+        public ViewResult Index()
+        {
+            return View(db.Class.ToList());
+        }
+
+        //
+        // GET: /Class/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Class classes = db.Class.Find(id);
+            return View(classes);
+        }
+
+        //
+        // GET: /Class/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Class/Create
+
+        [HttpPost]
+        public ActionResult Create(Class classes)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Class.Add(classes);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            return View(classes);
+        }
+        
+        //
+        // GET: /Class/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            Class classes = db.Class.Find(id);
+            return View(classes);
+        }
+
+        //
+        // POST: /Class/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Class classes)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(classes).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(classes);
+        }
+
+        //
+        // GET: /Class/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            Class classes = db.Class.Find(id);
+            return View(classes);
+        }
+
+        //
+        // POST: /Class/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            Class classes = db.Class.Find(id);
+            db.Class.Remove(classes);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CourseManager/Controllers/CourseManagerController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CourseManager.Models;
+
+namespace CourseManager.Controllers
+{ 
+    public class CourseManagerController : Controller
+    {
+        private CourseManagerEntities1 db = new CourseManagerEntities1();
+
+        //
+        // GET: /CourseManager/
+
+        public ViewResult Index()
+        {
+            return View(db.CourseManager.ToList());
+        }
+
+        //
+        // GET: /CourseManager/Details/5
+
+        public ViewResult Details(int id)
+        {
+            CourseManagers coursemanagers = db.CourseManager.Find(id);
+            return View(coursemanagers);
+        }
+
+        //
+        // GET: /CourseManager/Create
+
+        public ActionResult Create()
+        {
+            var classes = db.Classes.ToList();
+            ViewBag.Classes = classes;
+
+            var teachers = db.Teachers.ToList();
+            ViewBag.Classes =teachers ;
+
+            var course = db.Course.ToList();
+            ViewBag.Courses = course;
+            return View();
+        }
+        //
+        // POST: /CourseManager/Create
+
+        [HttpPost]
+        public ActionResult Create(CourseManagers coursemanager)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CourseManager.Add(coursemanagers);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(coursemanager);
+        }
+        
+        //
+        // GET: /CourseManager/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            CourseManager coursemanagers = db.CourseManager.Find(id);
+            return View(coursemanagers);
+        }
+
+        //
+        // GET: /CourseManager/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            CourseManagers coursemanager = db.CourseManager.Find(id);
+            return View(coursemanager);
+        }
+
+        //
+        // POST: /CourseManager/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            CourseManagers coursemanager = db.CourseManagers.Find(id);
+            db.CourseManager.Remove(coursemanagers);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
